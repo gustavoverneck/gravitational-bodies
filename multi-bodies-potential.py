9de31cebe69d8db68792d4825f780143537b0d18
Gustavo A. Verneck <gustavoverneck@gmail.com>
2023-04-07 10:07:49 -0300

    Add files via upload

--- multi-bodies-potential.py
+++ multi-bodies-potential.py
@@ -1,11 +1,7 @@
 '''
 	SIMULADOR DE N-CORPOS INTERAGENTES GRAVITACIONALMENTE: "FORMAÇÃO DE ESTRELAS"
 	
-	PROBLEMAS: TEMPO COMPUTACIONAL E ERROS PELO MÉTODO DE EULER
-	
-	IDEIA FUTURA: EVOLUIR O SISTEMA TEMPORALMENTE ATRAVÉS DE UM POTENCIAL MÉDIO QUE TAMBÉM VARIA COM O TEMPO
-
-	BUGS: PARECE ESTAR CRIANDO AS PARTÍCULAS 2X APÓS COLISÃO - OU TENTAR CONRRIGIR OU SÓ DIVIDIR A MASSA POR 2
+	PROPOSTA: CALCULAR O POTENCIAL MÉDIO E EVOLUIR TEMPORALMENTE, RECALCULANDO O POTENCIAL A CADA CICLO
 
 '''
 
@@ -40,23 +36,25 @@ class Mass:
 		print("tempo: ", global_time, " ; Particula criada: ", "massa: ",self.m, "; (",self.x,",",self.y,")", " ; v: (", self.vx, ",", self.vy, ")")
 	
 	def move(self):
-		self.vx += self.Fx / self.m * dt
-		self.vy += self.Fy / self.m * dt
+		self.vx += self.Fx * dt #/self.m
+		self.vy += self.Fy * dt #/self.m
 		self.x += self.vx*dt
 		self.y += self.vy*dt
 		self.Fx = 0
 		self.Fy = 0
 
-	def force(self, P, q):
-		R = (np.sqrt((q.x - P.x)**2 + (q.y - P.y)**2))
+	def force(self, phi):
 		bateu = False
-		if R < self.m/5:
+		'''if R < self.m/5:	# ADICIONAR VERIFICAÇÃO DE COLISÃO
 			self.colision(P, q)
 			bateu = True
 		if R > 0 and not bateu:
 			self.Fx = G*self.m * P.m * (self.x - P.x)/R**2
 			self.Fy = G*self.m * P.m * (self.y - P.y)/R**2
-		
+		'''
+		self.Fx = phi/self.x
+		self.Fy = phi/self.y
+
 	def show(self):
 		ax.plot(self.x, self.y, 'ob', markersize=self.m/5)
 
@@ -75,6 +73,18 @@ class Mass:
 		elif np.sqrt(self.x**2) >= space_size:
 			self.vx = -self.vx
 
+def campo_medio(particulas):
+	phi = 0
+	M = 0
+	for p in particulas:
+		M += p.m
+		R = np.sqrt((p.x)**2 + (p.y)**2)
+		phi_p = G*p.m/R
+		phi += phi_p
+	phi = phi/M
+	return phi
+
+
 def animate(U):
 	global particulas, global_time
 	ax.clear()
@@ -83,11 +93,10 @@ def animate(U):
 	global_time = round(U*dt, 2)
 	ax.set_title("number of particles: {} ; time: {}".format(len(particulas), global_time))
 	
-	for q in particulas:
-		for p in particulas:
-			if q != p:
-				p.force(p, q)
+	phi = campo_medio(particulas)
+	
 	for p in particulas:
+		p.force(phi)
 		p.verify_out_of_limits()
 		p.move()
 		p.show()
